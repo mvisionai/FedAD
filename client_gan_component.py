16bbc7635fb45be86e06a19d6a51b2c354ddc235
mvisionai <vision2bernard2015@gmail.com>
2020-11-24 21:32:09 +0800

    aae training

--- client_gan_component.py
+++ client_gan_component.py
@@ -16,7 +16,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from IPython.display import HTML
-import itertools
 
 from model_custom import ClientDiscriminator,ClientEncoder,ClientDecoder,weights_init,ClientClassifier,ClientGenerator
 from utils import *
@@ -55,9 +54,12 @@ plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=
 
 # Create the generator
 netE = ClientEncoder(ngpu).to(device)
-netDe = ClientDecoder(ngpu).to(device)
+netDe = ClientEncoder(ngpu).to(device)
 
 # Handle multi-gpu if desired
+if (device.type == 'cuda') and (ngpu > 1):
+    netE = nn.DataParallel(netE, list(range(ngpu)))
+    netDe = nn.DataParallel(netDe, list(range(ngpu)))
 
 # Apply the weights_init function to randomly initialize all weights
 #  to mean=0, stdev=0.2.
@@ -115,7 +117,6 @@ G_losses = []
 D_losses = []
 iters = 0
 
-
 print("Starting Training Loop...")
 # For each epoch
 for epoch in range(num_epochs):
@@ -129,63 +130,74 @@ for epoch in range(num_epochs):
         ###########################
         ## Train with all-real batch
         netD.zero_grad()
-        netDe.zero_grad()
-        netE.zero_grad()
-
         # Format batch
-        real_img = data[0].to(device)
-        b_size = real_img.size(0)
+        real_cpu = data[0].to(device)
+        b_size = real_cpu.size(0)
         label = torch.full((b_size,), real_label, dtype=torch.float, device=device).to(device)
-        
-        encoder_img = netD(real_img)
-
-        print("My size",encoder_img.size())
-        decoder_img = netDe(encoder_img)
-
-        # Forward pass generate batch through De
-        errRecons = mae_loss(real_img,decoder_img)
-
-        #Calculate the gradients for generated img
-        errRecons.backward()
-
-        optimizerDe.step()
-        optimizerE.step()
-
-        noise = torch.randn(b_size, nz, 1, 1, device=device)
-
-        # Forward pass  through Discriminator
-        output_real = netD(noise).view(-1)
-        output_fake = netD(encoder_img).view(-1)
+        # Forward pass real batch through D
+        output = netD(real_cpu).view(-1)
         # Calculate loss on all-real batch
-        errD_real = adversarial_loss(output_real, label)
-        label.fill_(fake_label)
-        errD_fake = adversarial_loss(output_fake, label)
+        errD_real = criterion(output, label)
+        # Calculate gradients for D in backward pass
+        errD_real.backward()
 
+        D_x = output.mean().item()
+
+        ## Train with all-fake batch
+        # Generate batch of latent vectors
+        noise = torch.randn(b_size, nz, 1, 1, device=device)
+        # Generate fake image batch with G
+        fake = netG(noise)
+        label.fill_(fake_label)
+        # Classify all fake batch with D
+        output = netD(fake.detach()).view(-1)
+        # Calculate D's loss on the all-fake batch
+        errD_fake = criterion(output, label)
+        # Calculate the gradients for this batch
+        errD_fake.backward()
+
+        D_G_z1 = output.mean().item()
+        # Add the gradients from the all-real and all-fake batches
         errD = errD_real + errD_fake
 
-        # Calculate gradients for D in backward pass
-        errD.backward()
-
+        del errD_fake
+        del errD_real
+        # Update D
         optimizerD.step()
 
+        ############################
+        # (2) Update G network: maximize log(D(G(z)))
+        ###########################
+        netG.zero_grad()
+        label.fill_(real_label)  # fake labels are real for generator cost
+        # Since we just updated D, perform another forward pass of all-fake batch through D
+        output = netD(fake).view(-1)
+        # Calculate G's loss based on this output
+        errG = criterion(output, label)
+        # Calculate gradients for G
+        errG.backward()
+
+
+        D_G_z2 = output.mean().item()
+        # Update G
+        optimizerG.step()
 
         # Output training stats
         if i % 50 == 0:
-            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f'
+            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
                   % (epoch, num_epochs, i, len(dataloader),
-                     errD.item(), errRecons.item(), errD))
+                     errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
 
 
 
         # Save Losses for plotting later
-        G_losses.append(errRecons.item())
+        G_losses.append(errG.item())
         D_losses.append(errD.item())
-
+        del errG
         # Check how the generator is doing by saving G's output on fixed_noise
         if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
             with torch.no_grad():
-                encoder_track = netE(real_batch[0].to(device)[:64])
-                fake = netDe(encoder_track).detach().cpu()
+                fake = netG(fixed_noise).detach().cpu()
             img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
             plt.subplot(1, 2, 2)
             plt.axis("off")
--- model_custom.py
+++ model_custom.py
@@ -28,6 +28,39 @@ def weights_init(m):
 
 
 
+class ClientGenerator(nn.Module):
+    def __init__(self, ngpu):
+        super(ClientGenerator, self).__init__()
+        self.ngpu = ngpu
+        self.encoder = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d( nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            # input is Z, going into a convolution
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 64 x 64
+        )
+
+    def forward(self, input):
+        return self.encoder(input)
+
+
+
 class ClientEncoder(nn.Module):
     def __init__(self, ngpu):
         super(ClientEncoder, self).__init__()
@@ -45,6 +78,8 @@ class ClientEncoder(nn.Module):
             nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True)
+
+
         )
 
     def forward(self, input):
@@ -102,3 +137,23 @@ class ClientDiscriminator(nn.Module):
 
     def forward(self, input):
         return self.main(input)
+
+
+class ClientClassifier(nn.Module):
+    def __init__(self):
+        super(ClientClassifier, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
