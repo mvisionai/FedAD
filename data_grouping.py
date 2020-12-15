3ddef63f534ab0f7034c98b0e210416b6000ee66
mvisionai <vision2bernard2015@gmail.com>
2020-12-15 17:23:57 +0800

    move mri file

--- data_grouping.py
+++ data_grouping.py
@@ -2,12 +2,14 @@ import os
 import tqdm
 import pandas as pd
 import  numpy as np
+import shutil
 
 #data[(data['NACCID']==x[0]) | (data['MRIYR'] == x[1])]
 csv_path_mri="E:\\fed_work2\cobbinah11042020mri.csv"
 csv_path_uds="E:\\fed_work2\cobbinah11042020.csv"
 path_save = "E:\\fed_work2\mri_p.npy"
-
+destination_base = "E:\\fed_work2\\NAC"
+src_base = "E:\\fed_work2\\NACC"
 compare_path = "C:\\NACC"
 
 mri_headers =["NACCID","NACCMRFI","MRIYR"]
@@ -33,6 +35,7 @@ def extracted_mri():
     refined_data = uds_data.apply(lambda x: filter_mri(x, mri_data), axis=1)
 
     numpy_convert = np.asarray(refined_data.dropna())
+
     np.save(path_save,numpy_convert)
 
 def read_mri_npy():
@@ -45,21 +48,44 @@ def move_mri(data):
     for dt in data:
 
         dt= np.asarray(dt)
-        print(dt[5])
+        mri_split = str(dt[5]).split()
 
-        exit(1)
+
         m_class = dt[4]
-        m_mri = dt[6]
-        class_label=""
+        m_mri = mri_split[1]
 
-        if m_class == 1:
-            class_label = "NC"
-        elif m_class == 3:
-            class_label = "MCI"
-        elif m_class == 4:
-            class_label = "AD"
+        try:
+
+         if m_class in [1,3,4]:
+
+           move_util(m_class,m_mri)
+
+        except Exception:
+            raise
 
 
+
+def move_util(m_class,src_file):
+
+    if m_class == 1:
+        class_label = "NC"
+
+    elif m_class == 3:
+        class_label = "MCI"
+
+    elif m_class == 4:
+        class_label = "AD"
+
+    destine_path = os.path.join(destination_base,class_label,src_file)
+    source_path = os.path.join(src_base,src_file)
+
+    try:
+
+     #shutil.move(source_path, destine_path)
+     print(destine_path,source_path)
+
+    except Exception:
+        pass
 
 if __name__ == "__main__":
     #extracted_mri()
